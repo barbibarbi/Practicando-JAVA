17acaf1bd584cc942654733407d0fae7a84ea0c0
Barbi <103278474+barbibarbi@users.noreply.github.com>
2025-09-28 21:44:24 -0300

    Update holamundo.java

--- Hola mundo/holamundo.java
+++ Hola mundo/holamundo.java
@@ -1,6 +1,8 @@
-public class holamundo {
-    public static void main(String[] args) 
-    {
-        System.out.println("Hola, mundo!");
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
     }
 }
